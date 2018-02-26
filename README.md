d8e04b2f5d138b1a600430bf80030aa67611ce31
swentel <swentel@realize.be>
2018-02-26 15:24:21 +0100

    Initial commit

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 Webmention.io is a hosted service created to easily handle webmentions (and legacy pingbacks) on any web page. This Drupal 8 modules exposes an endpoint (/webmention/notify) to receive pingbacks and webmentions via this service. Pingbacks are also validated to make sure that the source URL has a valid link to the target.
 
+You need an account at https://webmention.io. As soon as one webmention is recorded, you can set the the webhook to http://your_domain/webmention/notify.
+
 ## Installation
 
 - Go to admin/modules and toggle 'Webmention IO'
@@ -15,20 +17,21 @@ Webmention.io is a hosted service created to easily handle webmentions (and lega
   ```
 
 - Pingbacks and webmentions are stored in a content type callback 'Backlinks' as user 1.
-  An overview of collected links is available at admin/content/backlinks.
-  
+  An overview of collected links is available at admin/content/backlinks. Configure admin/content to exclude them from
+  that view (if wanted).
+
 ## Configuration
 
 Two settings can be configured by adding lines to settings.php
-  
+
   - Logging the payload in watchdog:
-  
+
   ```
   $settings['webmention_io_log_payload'] = TRUE;
   ```
-   
+
   - Assigning a different user id for the backlink:
-  
+
   ```
   $settings['webmention_io_uid'] = 321;
   ```
---
+++ src/Controller/WebmentionIoController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Drupal\webmention_io\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Site\Settings;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class WebmentionIoController extends ControllerBase {
+
+  /**
+   * Routing callback: receive webmentions and pingbacks from Webmention.io.
+   */
+  public function endpoint() {
+
+    // Default response code and message.
+    $response_code = 400;
+    $response_message = 'Bad request';
+
+    // Check if there's any input from the webhook.
+    $input = file('php://input');
+    $input = is_array($input) ? array_shift($input) : '';
+    $mention = json_decode($input, TRUE);
+
+    // Check if this is a forward pingback.
+    if (empty($mention)) {
+      if (!empty($_POST['source']) && !empty($_POST['target'])) {
+        $mention = [];
+        $mention['source'] = $_POST['source'];
+        $mention['post'] = [];
+        $mention['post']['type'] = 'pingback';
+        $mention['post']['wm-property'] = 'pingback';
+        $mention['target'] = $_POST['target'];
+      }
+    }
+
+    // Debug.
+    if (Settings::get('webmention_io_log_payload', FALSE)) {
+      $this->getLogger('webmention_io')->notice('input: @input', ['@input' => print_r($input, 1)]);
+    }
+
+    if (!empty($mention)) {
+
+      // Debug.
+      if (Settings::get('webmention_io_log_payload', FALSE)) {
+        $this->getLogger('webmention_io')->notice('object: @object', ['@object' => print_r($mention, 1)]);
+      }
+
+      if ($this->validateSource($mention['source'], $mention['target'])) {
+
+        $response_code = 202;
+        $response_message = 'Webmention was successful';
+
+        $values = [
+          'title' => 'Backlink: ' . $mention['source'],
+          'type' => 'backlinks',
+          'uid' => Settings::get('webmention_io_uid', 1),
+          // Remove the base url.
+          'field_webmention_target' => ['value' => str_replace(\Drupal::request()->getSchemeAndHttpHost(), '', $mention['target'])],
+          'field_webmention_source' => ['value' => $mention['source']],
+          'field_webmention_type' => ['value' => $mention['post']['type']],
+          'field_webmention_property' => ['value' => $mention['post']['wm-property']]
+        ];
+
+        // Save the node.
+        $node = $this->entityTypeManager()->getStorage('node')->create($values);
+        $node->save();
+      }
+
+    }
+
+    $response = ['result' => $response_message];
+    return new JsonResponse($response, $response_code);
+  }
+
+  /**
+   * Validates that target is linked on source.
+   *
+   * @param $source
+   * @param $target
+   *
+   * @return bool
+   */
+  protected function validateSource($source, $target) {
+    $valid = FALSE;
+
+    $content = file_get_contents($source, $target);
+    file_put_contents('/tmp/content', $content);
+    if ($content && strpos($content, $target) !== FALSE) {
+      $valid = TRUE;
+    }
+
+    return $valid;
+  }
+
+}
